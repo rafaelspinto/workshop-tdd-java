1c65379352b5a321c8bb14f6f66cbf50b952b232
Rafael Pinto <santospinto.rafael@gmail.com>
2018-02-20 08:50:52 +0000

    Added Ignore Annotation to CalculatorBddTest

--- src/test/java/workshop/calculator/CalculatorBddTest.java
+++ src/test/java/workshop/calculator/CalculatorBddTest.java
@@ -4,7 +4,9 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
+import org.junit.Ignore;
 
+@Ignore
 public class CalculatorBddTest {
 
   private Calculator calculator;
